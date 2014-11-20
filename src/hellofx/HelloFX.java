ba461166b61a96c2e6bb0e6c198f8b7856166e02
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame14-Th>
2014-11-20 11:01:46 +0200

    New project

--- src/hellofx/HelloFX.java
+++ src/hellofx/HelloFX.java
@@ -5,13 +5,18 @@
  */
 package hellofx;
 
+import java.awt.BasicStroke;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javax.swing.border.StrokeBorder;
 
 /**
  *
@@ -21,22 +26,56 @@ public class HelloFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World! "+event.getEventType().toString());
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // Note: Root layout should contain only other layouts.
+        
+        // Main layout is root
+        VBox root = new VBox();
+
+        root.setStyle("-fx-padding:0,10,0,10"); // :top,right,bottom,left
+        
+        // Query components
+        Label labelName = new Label("Name");
+        TextField fieldName = new TextField(); // single line, Area for multi
+        Label labelAddress = new Label("Address");
+        TextField fieldAddress = new TextField(); // single line, Area for multi
+        Label labelPhone = new Label("Phone");
+        TextField fieldPhone = new TextField(); // single line, Area for multi
+        
+        // Layout for Query components
+        VBox queries = new VBox();
+      
+        queries.setStyle("-fx-padding:10;-fx-spacing:10");
+        
+        // Add Query components to layout
+        queries.getChildren().add(labelName);
+        queries.getChildren().add(fieldName);
+        queries.getChildren().add(labelAddress);
+        queries.getChildren().add(fieldAddress);
+        queries.getChildren().add(labelPhone);
+        queries.getChildren().add(fieldPhone);
+        
+        // Add queries layout to root
+        root.getChildren().add(queries);
+        
+        // Layout for action Button components
+        HBox buttons = new HBox();
+
+        buttons.setStyle("-fx-padding:10;-fx-spacing:10");
+        
+        // Button components for actions
+        Button buttonClose = new Button("Close");
+        Button buttonSave = new Button("Save");
+        
+        // Add Buttons to layout
+        buttons.getChildren().add(buttonClose);
+        buttons.getChildren().add(buttonSave);
+        
+        // Add buttons layout to root
+        root.getChildren().add(buttons);
         
         Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("IDEA");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
